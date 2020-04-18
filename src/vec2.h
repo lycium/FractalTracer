0ded8e0d04340c92a992fd762483cb8f614c0be6
Thomas Ludwig (8700k) <thomas.ludwig@gmail.com>
2020-04-18 23:20:18 +0200

    Changed vec2, vec3 and Dual classes to use template argument real_type to avoid clash with real.

--- src/vec2.h
+++ src/vec2.h
@@ -7,21 +7,21 @@
 
 
 
-template<typename real>
+template<typename real_type>
 class vec2
 {
 public:
-	real x, y;
+	real_type x, y;
 
 	inline vec2() { }
 	inline vec2(const vec2 & v) : x(v.x), y(v.y) { }
-	inline vec2(real v) : x(v), y(v) { }
-	inline vec2(real x_, real y_) : x(x_), y(y_) { }
+	inline vec2(real_type v) : x(v), y(v) { }
+	inline vec2(real_type x_, real_type y_) : x(x_), y(y_) { }
 
 	inline vec2 operator+(const vec2 & rhs) const { return vec2(x + rhs.x, y + rhs.y); }
 	inline vec2 operator-(const vec2 & rhs) const { return vec2(x - rhs.x, y - rhs.y); }
-	inline vec2 operator*(const real rhs) const { return vec2(x * rhs, y * rhs); }
-	inline vec2 operator/(const real rhs) const { return vec2(x / rhs, y / rhs); }
+	inline vec2 operator*(const real_type rhs) const { return vec2(x * rhs, y * rhs); }
+	inline vec2 operator/(const real_type rhs) const { return vec2(x / rhs, y / rhs); }
 
 	inline vec2 operator*(const vec2 & rhs) const { return vec2(x * rhs.x, y * rhs.y); }
 
@@ -31,7 +31,7 @@ public:
 
 	inline const vec2 & operator+=(const vec2 & rhs) { x += rhs.x; y += rhs.y; return *this; }
 	inline const vec2 & operator-=(const vec2 & rhs) { x -= rhs.x; y -= rhs.y; return *this; }
-	inline const vec2 & operator*=(const real rhs) { x *= rhs; y *= rhs; return *this; }
+	inline const vec2 & operator*=(const real_type rhs) { x *= rhs; y *= rhs; return *this; }
 };
 
 using vec2i = vec2<int>;
@@ -44,16 +44,16 @@ using DualVec2f = vec2<Dual2f>;
 using DualVec2d = vec2<Dual2d>;
 
 
-template<typename real>
-inline real dot(const vec2<real> & lhs, const vec2<real> & rhs) { return lhs.x * rhs.x + lhs.y * rhs.y; }
+template<typename real_type>
+inline real_type dot(const vec2<real_type> & lhs, const vec2<real_type> & rhs) { return lhs.x * rhs.x + lhs.y * rhs.y; }
 
 
-template<typename real>
-inline real length(const vec2<real> & v) { return length(v); }
+template<typename real_type>
+inline real_type length(const vec2<real_type> & v) { return length(v); }
 
 
-template<typename real>
-inline vec2<real> normalise(const vec2<real> & v, const real len = 1) { return v * (len / length(v)); }
+template<typename real_type>
+inline vec2<real_type> normalise(const vec2<real_type> & v, const real_type len = 1) { return v * (len / length(v)); }
 
 
 inline static vec2f toVec2f(const vec2d & v) { return vec2f((float)v.x, (float)v.y); }
