9613442880b7133ce86896c21ad273d6b637b032
Thomas Ludwig (8700k) <thomas.ludwig@gmail.com>
2020-05-11 00:24:42 +0200

    Fixes and improvements to MandalayKIFS: fundamentally scalar params converted from dual, fixed rotation, more const-ness, using length2

--- src/MandalayKIFS.h
+++ src/MandalayKIFS.h
@@ -12,13 +12,13 @@ struct DualMandalayKIFSIteration final : public IterationFunction
 	real scale = 2;
 	real min_r2 = 0;
 	real fix_r2 = 1;
-	Dual3r fold = 1;
-	Dual3r xy_tower = 1;
-	Dual3r z_tower = 0;
-	DualVec3r rot_m1 = DualVec3r{ 1.0f, 0.0f, 0.0f };
-	DualVec3r rot_m2 = DualVec3r{ 0.0f, 1.0f, 0.0f };
-	DualVec3r rot_m3 = DualVec3r{ 0.0f, 0.0f, 1.0f };
-	DualVec3r c = { 0.0f, 0.0f, 0.0f };
+	real fold = 1;
+	real xy_tower = 1;
+	real z_tower = 0;
+	vec3r rot_m1 = { 1, 0, 0 };
+	vec3r rot_m2 = { 0, 1, 0 };
+	vec3r rot_m3 = { 0, 0, 1 };
+	DualVec3r c  = { 0, 0, 0 };
 	bool julia_mode = true;
 
 
@@ -30,54 +30,48 @@ struct DualMandalayKIFSIteration final : public IterationFunction
 
 	virtual void eval(const DualVec3r & p_in, DualVec3r & p_out) const noexcept override final
 	{
-		DualVec3r p = p_in;
-
 		// Rotate
-		p = p * rot_m1 + p * rot_m2 + p * rot_m3;
-		
+		DualVec3r p = DualVec3r(
+			dot(p_in, rot_m1),
+			dot(p_in, rot_m2),
+			dot(p_in, rot_m3));
+
 		p = DualVec3r(fabs(p.x), fabs(p.y), fabs(p.z));
 
 		// Octahedral fold
-		if (p.y.v[0] > p.x.v[0])
-			p = DualVec3r(p.y, p.x, p.z);
-		if (p.z.v[0] > p.y.v[0])
-			p = DualVec3r(p.x, p.z, p.y);
-		if (p.y.v[0] > p.x.v[0])
-			p = DualVec3r(p.y, p.x, p.z);
+		if (p.y.v[0] > p.x.v[0]) p = DualVec3r(p.y, p.x, p.z);
+		if (p.z.v[0] > p.y.v[0]) p = DualVec3r(p.x, p.z, p.y);
+		if (p.y.v[0] > p.x.v[0]) p = DualVec3r(p.y, p.x, p.z);
 
 		// ABoxKali-like abs folding
-		Dual3r fx = fold * -2 + p.x;
+		const Dual3r fx = p.x + fold * -2;
 
 		// Edges
-		DualVec3r q;
-		q.x = fold - fabs(-fold + p.x);
-		q.y = fold - fabs(-fold + p.y);
-		if (z_tower.v[0] > 0)
-			q.z = z_tower - fabs(-fold + p.z);
-		else
-			q.z = z_tower + p.z;
+		const DualVec3r q0(
+			-fabs(p.x - fold) + fold,
+			-fabs(p.y - fold) + fold,
+			((z_tower > 0) ? -fabs(p.z - fold) : p.z) + z_tower);
 
-		Dual3r g = xy_tower;
-		Dual3r gy = g + p.y;
+		const Dual3r g  = xy_tower;
+		const Dual3r gy = g + p.y;
+
+		DualVec3r q = q0;
 		if (fx.v[0] > 0 && fx.v[0] > p.y.v[0])
 		{
 			if (fx.v[0] > gy.v[0])
 			{
 				// Top
 				q.x = q.x + g;
-				q.y = fold - fabs(g - fold + p.y);
+				q.y = -fabs(g - fold + p.y) + fold;
 			}
 			else
 			{
 				// Edges
 				q.x = -p.y;
-				q.y = fold - fabs(fold * -3 + p.x);
+				q.y = -fabs(p.x + fold * -3) + fold;
 			}
 		}
 		p = q;
-
-		//f (i < ColorIterations) 
-		//	orbitTrap = min(orbitTrap, abs(vec4(p.xyz, r2)));
 
 		sphereFold(p);
 
@@ -93,10 +87,10 @@ struct DualMandalayKIFSIteration final : public IterationFunction
 		return new DualMandalayKIFSIteration(*this);
 	}
 
-protected:
+private:
 	inline DualVec3r sphereFold(const DualVec3r & p_in) const
 	{
-		const real r2 = p_in.x.v[0] * p_in.x.v[0] + p_in.y.v[0] * p_in.y.v[0] + p_in.z.v[0] * p_in.z.v[0];
+		const real r2 = length2(p_in);
 		return
 			(r2 < min_r2) ? p_in * (fix_r2 / min_r2) : // linear inner scaling
 			(r2 < fix_r2) ? p_in * (fix_r2 / r2) : // this is the actual sphere inversion
