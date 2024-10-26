32843cded1c4b89f54535a86e3a0ff8faff7cbeb
Thomas Ludwig (8700k) <thomas.ludwig@gmail.com>
2024-10-26 05:28:05 +0200

    Allow VS Code on Windows, code consistency edits for Lambdabulb.h

--- src/formulas/Lambdabulb.h
+++ src/formulas/Lambdabulb.h
@@ -2,18 +2,22 @@
 
 #include "scene_objects/DualDEObject.h"
 
+
+
 // Lambdabulb formula
 // Based on modifications by machina-infinitum
 // Ref.: https://www.fractalforums.com/mandelbulb-renderings/lambdabulb/
 struct DualLambdabulbIteration final : public IterationFunction
 {
-	real power = 4;//2-5 should look good
 	const real phase = 1.815142;
 	const real sin_phase = sin(phase);
 	const real cos_phase = cos(phase);
+
+	real power = 4; // 2-5 should look good
 	DualVec3r c = { 1.035f, -0.317f, 0.013f };
 
-	virtual void init(const DualVec3r& p_0) noexcept override final
+
+	virtual void init(const DualVec3r & p_0) noexcept override final
 	{
 
 	}
@@ -27,42 +31,40 @@ struct DualLambdabulbIteration final : public IterationFunction
 
 	virtual real getPower() const noexcept override final { return power; }
 
-	virtual IterationFunction* clone() const override final
+	virtual IterationFunction * clone() const override final
 	{
 		return new DualLambdabulbIteration(*this);
 	}
 
 protected:
-
-	inline DualVec3r triplexPow(const DualVec3r& z, const real& power, const Dual3r& phase) const
+	inline DualVec3r triplexPow(const DualVec3r& z, const real & power, const Dual3r & phase) const
 	{
-#if 0		// original trig version, arbitrary power
-
-		const Dual3r r = length(z);
+#if 0
+		// original trig version, arbitrary power
+		const Dual3r r     = length(z);
 		const Dual3r theta = atan2(z.y(), z.x());
-		const Dual3r phi = acos(z.z() / r);
-
-		const Dual3r r_p = pow(r, power);
+		const Dual3r phi   = acos(z.z() / r);
+		const Dual3r r_p     = pow(r, power);
 		const Dual3r theta_p = theta * power;
-		const Dual3r phi_p = phase + phi * power;
+		const Dual3r phi_p   = phase + phi * power;
 
 		return DualVec3r(
 			sin(phi_p) * cos(theta_p),
 			sin(phi_p) * sin(theta_p),
 			cos(phi_p)
 		) * r_p;
-
-#else		// polynomial version, hardcoded to power 4
+#else
+		// polynomial version, hardcoded to power 4
 
 		// wxmaxima: factor(trigexpand(cos(4*atan2(y,x)))) rsp. sin
-		const auto x2 = z.x() * z.x();
-		const auto y2 = z.y() * z.y();
-		const auto xy2 = real(2) * (z.x() * z.y());
-		const auto y2mx2 = y2 - x2;
-		const auto y2px2 = y2 + x2;
+		const auto x2     = z.x() * z.x();
+		const auto y2     = z.y() * z.y();
+		const auto xy2    = z.x() * z.y() * 2;
+		const auto y2mx2  = y2 - x2;
+		const auto y2px2  = y2 + x2;
 		const auto y2px22 = y2px2 * y2px2;
 		const auto cos_theta = (y2mx2 - xy2) * (y2mx2 + xy2) / y2px22;
-		const auto sin_theta = real(-2) * (xy2 * y2mx2) / y2px22;
+		const auto sin_theta = (xy2 * y2mx2) / y2px22 * -2;
 
 		// wxmaxima: at(factor(expand(trigexpand(factor(trigexpand(cos(phase + 4 * acos(z / sqrt(x^2+y^2+z^2)))))))), [cos(phase) = c, sin(phase) = s]); rsp sin
 		// (c*z^4-4*s*sqrt(y^2+x^2)*z^3-6*c*y^2*z^2-6*c*x^2*z^2+4*s*y^2*sqrt(y^2+x^2)*z+4*s*x^2*sqrt(y^2+x^2)*z+c*y^4+2*c*x^2*y^2+c*x^4)/(z^2+y^2+x^2)^2
@@ -81,12 +83,10 @@ protected:
 			sin_phi * sin_theta,
 			cos_phi
 		);// * r22;
-
 #endif
-
 	}
 
-	inline DualVec3r triplexMult(const DualVec3r& z1, const DualVec3r& z2) const
+	inline DualVec3r triplexMult(const DualVec3r & z1, const DualVec3r & z2) const
 	{
 		const Dual3r r1 = length(z1);
 		const Dual3r r2 = length(z2);
